d5e0e191cdd6dd34f2aa5c8bdd4cd69ab34ab9d3
ramakrishnareddy9012 <ramakrishnareddy9012@gmail.com>
2024-11-01 17:46:55 +0530

    Uploading code: N - S - C.c

---
+++ N - S - C.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    for(int n = A + 1; n < B; n++)
+    {
+        printf("%d %d %d\n", n, n * n, n * n * n);
+    }
+    return 0;
+}
