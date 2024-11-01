6ae7071fc28d04f5be8976c03dacb21a63f120ee
ramakrishnareddy9012 <ramakrishnareddy9012@gmail.com>
2024-11-01 12:00:34 +0530

    Uploading code: Air Quality Index.c

---
+++ Air Quality Index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int aqui;
+    scanf("%d", &aqui);
+    if(aqui<100)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
