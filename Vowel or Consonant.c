900042b540dc05cde03e7fb01f436717fea95c29
ramakrishnareddy9012 <ramakrishnareddy9012@gmail.com>
2024-11-07 21:59:26 +0530

    Uploading code: Vowel or Consonant.c

--- Vowel or Consonant.c
+++ Vowel or Consonant.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-#include<ctype.h>
 int main()
 {
-    char ch;
-    scanf("%c",&ch);
-    ch = tolower(ch);
-    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+     char ch;
+    scanf("%c", &ch);
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'|| ch 
+    == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
     {
-        printf("VOWEL\n");
+    printf("VOWEL\n");
     }
     else
     {
